0f0df41fd3a475e4d5c531f220c4034f75ba1b17
dreamshader <dreamshader@gmx.de>
2018-11-19 22:51:08 +0100

    added dsGPIO support and more sample calls in test program

--- src/rtcTest.cpp
+++ src/rtcTest.cpp
@@ -38,8 +38,13 @@ int main(int argc, char *argv[])
     uint16_t spiDelay    = DS324_SPI_DELAY;
     uint32_t maxDataSize = DS3234_DATA_BUF_SIZE;
 
+    time_t currSecs;
+    struct tm *pNow;
+
     dsDS3234 clock1;
-    struct _ds3234_timestruct_ now;
+
+    struct _ds3234_timestruct_     currTime;
+    struct _ds3234_alarm_settings_ alarmSettings;
 
 #ifdef _USE_PIGPIO_
     if( (retVal = gpioInitialise()) >= 0)
@@ -59,10 +64,60 @@ int main(int argc, char *argv[])
 
             if( retVal >= 0 )
             {
-                retVal = clock1.readDate( &now );
-
+                retVal = clock1.readDate( &currTime );
                 printf("readDate: retVal = %d\n", retVal );
-            }
+
+                time(&currSecs);
+                pNow = localtime(&currSecs);
+
+                currTime.second  = pNow->tm_sec;         /* seconds */
+                currTime.minute  = pNow->tm_min;         /* minutes */
+                currTime.hour    = pNow->tm_hour;        /* hours */
+
+                currTime.hour24  =  0;
+
+                currTime.wday    = pNow->tm_wday + 1;    /* day of the week */
+                currTime.date    = pNow->tm_mday;        /* day of the month */
+                currTime.month   = pNow->tm_mon + 1;     /* month */
+                currTime.year    = pNow->tm_year;        /* year */
+
+                currTime.century =  0;
+
+                retVal = clock1.writeDate( &currTime );
+                printf("writeDate: retVal = %d\n", retVal );
+
+                alarmSettings.alrmNo   = 1;
+                alarmSettings.alrmMode = 0;
+                alarmSettings.enable   = false;
+                alarmSettings.second   = 23;
+                alarmSettings.minute   = 10;
+                alarmSettings.hour     = 22;
+                alarmSettings.date     = 17;
+
+                retVal = clock1.setAlarm( &alarmSettings );
+                printf("setAlarm: retVal = %d\n", retVal );
+
+                retVal = clock1.readAlarm( &alarmSettings );
+                printf("writeDate: retVal = %d\n", retVal );
+
+                retVal = clock1.enableAlarm( 1 );
+                printf("enableAlarm: retVal = %d\n", retVal );
+
+                retVal = clock1.disableAlarm( 1 );
+                printf("disableAlarm: retVal = %d\n", retVal );
+
+                alarmSettings.alrmNo   = 1;
+                alarmSettings.alrmMode = 0;
+                alarmSettings.enable   = false;
+                alarmSettings.second   = 23;
+                alarmSettings.minute   = 10;
+                alarmSettings.hour     = 22;
+                alarmSettings.date     = 17;
+    
+                retVal = clock1.clearAlarm( &alarmSettings );
+                printf("clearAlarm: retVal = %d\n", retVal );
+
+            } 
 
 #ifdef _USE_PIGPIO_
         gpioTerminate();
